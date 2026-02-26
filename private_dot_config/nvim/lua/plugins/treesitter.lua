183b5b888876a0b11bf6446e68e4b45642b4c983
Tianwen Fu <kevin.futianwen@gmail.com>
2026-02-25 19:43:37 -0800

    Initial commit

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "latex",
+        "css",
+        "cuda",
+        "rust",
+        "markdown",
+        "markdown_inline",
+        "glsl",
+        "go",
+        "gitignore",
+        "make",
+        "ocaml",
+        "python",
+        "ssh_config",
+        "zsh",
+      },
+    },
+  },
+}
