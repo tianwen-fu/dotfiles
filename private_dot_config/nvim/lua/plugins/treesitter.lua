175101ec39756380419ed68c6e0f329fe5f8cb6a
Tianwen Fu <tianwenf@usc.edu>
2026-03-06 12:03:54 -0800

    chezmoi update

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,24 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "latex",
-        "css",
-        "cuda",
-        "rust",
-        "markdown",
-        "markdown_inline",
-        "glsl",
-        "go",
-        "gitignore",
-        "make",
-        "ocaml",
-        "python",
-        "ssh_config",
-        "zsh",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"latex",
+				"css",
+				"cuda",
+				"rust",
+				"markdown",
+				"markdown_inline",
+				"glsl",
+				"go",
+				"gitignore",
+				"make",
+				"ocaml",
+				"python",
+				"ssh_config",
+				"toml",
+				"zsh",
+			},
+		},
+	},
 }
